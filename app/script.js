b5b4006b9cefc3eeea413bb76a2b5a0161378659
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-02-19 03:18:14 +0600

    QuerySelector

--- app/script.js
+++ app/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+let message = document.querySelector('.message').textContent; // this will print the value of the class message in the index.html
+
+console.log(message) 
+
+document.querySelector('.number').textContent = 10
+document.querySelector('.score').textContent = 100
+
+//setting default input value of guess class
+document.querySelector('.guess').value = 3;
+console.log(document.querySelector('.guess').value)
